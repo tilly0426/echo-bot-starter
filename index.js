69c30507a00652a682489cd02a49e534077b4192
tilly0426 <tilly0426@yahoo.com.tw>
2017-07-16 18:57:04 +0800

    send message

--- index.js
+++ index.js
@@ -21,11 +21,40 @@ app.get('/facebook', function (req, res) {
     }
 })
 
+// handler receiving messages
 app.post('/facebook', function (req, res) {
-  console.log('query:', req.query);
-  console.log('body:', req.body);
-  res.send('hello facebook from POST');
+    let events = req.body.entry[0].messaging
+    for (i = 0; i < events.length; i++) {
+        let event = events[i]
+        if (event.message) {
+            if (event.message.text) {
+                sendMessage(event.sender.id, { text: event.message.text })
+            }
+        }
+    }
+    res.sendStatus(200)
 })
+
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    let options = {
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: FACEBOOK_ACCESS_TOKEN },
+        method: 'POST',
+        json: {
+            recipient: { id: recipientId },
+            message: message,
+        }
+    }
+    request(options, function (error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        }
+        else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    })
+}
 
 app.get('/line', function (req, res) {
   console.log('query:', req.query);
